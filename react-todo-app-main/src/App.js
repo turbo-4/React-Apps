26a1ead0eacdee0fa8a1c25e3dd21787ccc90970
Prateek Gupta <pgk57312@gmail.com>
2022-02-26 00:57:56 +0530

    My React Apps

---
+++ react-todo-app-main/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo"
+
+function App() {
+  return (
+    <div>
+      <h1>Todo List App</h1>
+      <hr/>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
